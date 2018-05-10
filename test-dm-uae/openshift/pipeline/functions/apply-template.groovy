01208ec804cb62ad88ce80bbb4cb672591fb2785
Raif Ahmed <rahmed@redhat.com>
2018-05-10 13:07:52 +0400

    changed jenkinsfile-6

--- test-dm-uae/openshift/pipeline/functions/apply-template.groovy
+++ test-dm-uae/openshift/pipeline/functions/apply-template.groovy
@@ -2,7 +2,12 @@
 
 def applyTemplate(project, templateFile, appName, appVersion, imageStreamName, customParameters, skipObjects) {
    echo "Applying template ${templateFile} in project ${project}. Application: ${appName}-${appVersion}"
+   openshift.withProject( project ) {
       echo "Additional parameters for template are ${customParameters}"
+      def models = openshift.process( readFile(templateFile), "-p APPLICATION_NAME=${appName}", "-p APP_VERSION=${appVersion}", customParameters )
+      echo "Discarding objects of type ${skipObjects}"
+
+   }
 
 }
 
