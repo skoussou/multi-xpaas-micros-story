ef9bab47a85fe32ab660712e8b315aea0a5820c4
Stelios Koussouris <skousou@gmail.com>
2018-05-14 13:37:08 +0100

    reverted to using the UAE Cluster nexus URLS

--- dm-micro/rest-api-resources/create-kie-container/ocp-delete-container-64-rest-api.sh
+++
@@ -1,8 +0,0 @@
-SERVICE=http://uaex-rules-api-poc.192.168.42.210.nip.io
-#KIE_USERNAME=stelios
-KIE_USERNAME=kieserver
-#KIE_PASSWORD=stelios123!
-KIE_PASSWORD=kieserver123!
-
-echo "curl -X DELETE -u ${KIE_USERNAME}:${KIE_PASSWORD} \"${SERVICE}/kie-server/services/rest/server/containers/payments\" -H  \"accept: application/xml\""
-curl -X DELETE -u "${KIE_USERNAME}:${KIE_PASSWORD}" "${SERVICE}/kie-server/services/rest/server/containers/payments" -H  "accept: application/xml"
